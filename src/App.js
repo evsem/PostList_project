ef52a9f4f86ae42340af1355414e5686198cc600
evsem <frukwork@gmail.com>
2022-01-26 17:56:43 +0300

    PostList_3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import PostItem from './Components/PostItem/PostItem'
 
 function App() {
+  let [posts, setPosts] = useState([
+    { id: 1, title: 'JS', body: 'Programming language' },
+    { id: 2, title: 'Python', body: 'Programming language' },
+    { id: 3, title: 'Swift', body: 'Programming language' },
+    { id: 4, title: 'Java', body: 'Programming language' },
+    { id: 5, title: 'PHP', body: 'Programming language' },
+  ])
   return (
     <div className="App">
-      <PostItem
-        props_postItem={{ id: 1, title: 'JS', body: 'Programming language' }}
-      />
+      {posts.map((post) => (
+        //Мапим массив posts, выводим каждый элемент массива posts в виде post. Затем мы присваимваем компоненту PostItem пропс(props_postItem), в который прокидываем элемент post. Ключ присваиваем по id(он должен быть всегда уникальным).
+        <PostItem props_postItem={post} key={posts.id} />
+      ))}
     </div>
   )
 }
--- src/Components/PostItem/PostItem.jsx
+++ src/Components/PostItem/PostItem.jsx
@@ -3,11 +3,16 @@ import MyButton from '../../UI/Buttons/MyButton'
 import classes from './PostItem.module.css'
 
 const PostItem = (props) => {
+  //
   return (
     <div className={classes.postItem_wrapper}>
       <div className={classes.postItem_textContainer}>
-        <h3 className={classes.postItem_textContainer__title}>{props.props_postItem.id}. {props.props_postItem.title}</h3>
-        <p className={classes.postItem_textContainer__body}>{props.props_postItem.body}</p>
+        <h3 className={classes.postItem_textContainer__title}>
+          {props.props_postItem.id}. {props.props_postItem.title}
+        </h3>
+        <p className={classes.postItem_textContainer__body}>
+          {props.props_postItem.body}
+        </p>
       </div>
       <MyButton>Delete</MyButton>
     </div>
